2674342367894dcfe24d040cde45184b8b24ba77
roxreis <rodrigoassuncao12@.com.br>
2019-09-27 12:10:18 -0300

    iniciando projeto

---
+++ index.php
@@ -0,0 +1,48 @@
+
+    <?php
+
+    $nomeSistema ="Loja dos Cursos"
+
+     ?> 
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Loja Virtual</title>
+</head>
+<body>
+
+    <header class="container d-flex justify-content-between align-items-center p-3 ">
+        <h1 id="logo"><?php echo $nomeSistema; ?></h1>
+     
+    <ul class="nav"> 
+       <li class="nav-item active">
+        <a class="nav-link" href="#">Cursos <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Cadastro</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Login</a>
+      </li>
+   
+    </ul>
+  </div>
+</nav>
+
+    </header>
+    
+    
+    
+
+    
+
+
+</body>
+</html>
